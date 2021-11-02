2b6c899a73220d67e219a36b634c5155eec3c310
Sam Li <samlee2015jp@gmail.com>
2021-11-02 11:49:12 +0900

    add

--- cs50-2019/lecture1-c/rockpaperscissors.c
+++ cs50-2019/lecture1-c/rockpaperscissors.c
@@ -13,11 +13,31 @@ int printRandoms(int lower, int upper, int count)
     return num;
 }
 
+void getRockPaperScisoorsResult(int randomValue, char subjectPredicate[])
+{
+    if (1 == randomValue)
+    {
+        printf("%s Rock!\n", subjectPredicate);
+    }
+    else if (2 == randomValue)
+    {
+        printf("%s Paper!\n", subjectPredicate);
+    }
+    else if (3 == randomValue)
+    {
+        printf("%s Scissors!\n", subjectPredicate);
+    }
+}
+
 int main()
 {
     int lower = 1, upper = 3, count = 1;
     srand(time(0));
     int randomValue = printRandoms(lower, upper, count);
-    printf("I generated the number is: %d\n", randomValue);
-    printf("You are Rock");
+    getRockPaperScisoorsResult(randomValue, "I am");
+    printf("Enter a number: 1:Rock, 2Paper, 3Scissors please!\n");
+    int userInput;
+    scanf("%d", &userInput);
+    getRockPaperScisoorsResult(userInput, "You are");
+
 }
