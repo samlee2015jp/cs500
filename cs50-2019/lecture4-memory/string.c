89685f07a35ebce2a63b6682820e34d994f2b09e
Sam Li <samlee2015jp@gmail.com>
2021-11-03 11:54:35 +0900

    string.h

---
+++ cs50-2019/lecture4-memory/string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "Geeks";
+
+	printf("address of str: %p\n", &str);
+	printf("str: %s\n", str);
+
+	return 0;
+}
---
+++ cs50-2019/lecture4-memory/teststr.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "string.h"
+
+int main()
+{
+	string s = "Hello, String";
+	printf("s: %s", s);
+}
